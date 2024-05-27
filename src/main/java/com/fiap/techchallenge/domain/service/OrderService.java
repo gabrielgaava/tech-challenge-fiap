ec3a23aabb1f81ed78afd7758e1c06950ccaaa52
gaava <nero.gava@gmail.com>
2024-05-26 21:48:55 -0300

    fix: Refactoring Order update status

--- src/main/java/com/fiap/techchallenge/domain/service/OrderService.java
+++ src/main/java/com/fiap/techchallenge/domain/service/OrderService.java
@@ -158,12 +158,49 @@ public class OrderService implements IOrderUseCase {
     {
         var order = IOrderRepository.getById(id);
 
-        if(order == null)
-            throw new EntityNotFoundException("Order", id);
+        // Order Not Found in Database
+        if(order == null) throw new EntityNotFoundException("Order", id);
 
-        if(status == null)
-            throw new IllegalArgumentException("Status cannot be null and have to be a valid status");
+        switch (status) {
 
+            // Invalid Status from Payload
+            case null: {
+                throw new IllegalArgumentException("Status cannot be null and have to be a valid status");
+            }
+
+            // Forbidden route usage
+            case RECEIVED: {
+                throw new IllegalArgumentException("To update to this status use payment route");
+            }
+
+            // IN_PREPARATION -> RECEIVED Validation
+            case IN_PREPARATION: {
+                if(order.getStatus() != RECEIVED)
+                    throw new IllegalArgumentException("Order must be in 'RECEIVED' status");
+                break;
+            }
+
+            // IN_PREPARATION -> RECEIVED Validation
+            case READY_TO_DELIVERY: {
+                if(order.getStatus() != IN_PREPARATION)
+                    throw new IllegalArgumentException("Order must be in 'IN_PREPARATION' status");
+                break;
+            }
+
+            // IN_PREPARATION -> RECEIVED Validation
+            case FINISHED: {
+                if(order.getStatus() != READY_TO_DELIVERY)
+                    throw new IllegalArgumentException("Order must be in 'READY_TO_DELIVERY' status");
+                break;
+            }
+
+            default: {
+                break;
+            }
+
+        }
+
+        // Order with the same status
         if(order.getStatus().equals(status))
             throw new OrderAlreadyWithStatusException(id, status);
 
@@ -223,6 +260,10 @@ public class OrderService implements IOrderUseCase {
         if(status.equals(CREATED)
             || status.equals(OrderStatus.FINISHED)
             || status.equals(OrderStatus.CANCELED))
+            return 0;
+
+        // Error retrieving paid at date
+        if(order.getPaidAt() == null)
             return 0;
 
         return Duration.between(order.getPaidAt(), LocalDateTime.now()).toSeconds();
