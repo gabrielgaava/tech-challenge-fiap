85a036129b747e48efa55141e8d61d6a4d74a4e0
speltriao <alexandrespeltri@gmail.com>
2024-07-28 21:56:33 -0300

    Fix: Category not null

--- src/main/java/com/fiap/techchallenge/handlers/rest/dto/CreateProductDTO.java
+++ src/main/java/com/fiap/techchallenge/handlers/rest/dto/CreateProductDTO.java
@@ -4,6 +4,7 @@ import com.fiap.techchallenge.domain.product.ProductCategory;
 import com.fiap.techchallenge.handlers.rest.constants.FieldValidationConstants;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,5 +30,6 @@ public class CreateProductDTO {
 	@DecimalMin(message = "must be greater than 0", value = "0", inclusive = false)
 	BigDecimal price;
 
+	@NotNull(message =  FieldValidationConstants.NOT_NULL)
 	ProductCategory category;
 }
